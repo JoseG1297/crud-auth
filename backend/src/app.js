853366b47aa82e47589b4f5cb6c95ddba72bb7df
JoseG1297 <mordo1309@gmail.com>
2025-02-01 16:27:27 -0600

    import routes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,15 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 
 // Mensajes de terminal para saber que está pasando
 app.use(morgan('dev'));
+
+// Para que express pueda entender los datos que vienen de un formulario
+app.use(authRoutes);
+
 
 export default app;
