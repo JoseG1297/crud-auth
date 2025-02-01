474c7e1347dd891cbb830cb22025c4eafa6cdb4b
JoseG1297 <mordo1309@gmail.com>
2025-02-01 17:02:11 -0600

    bcrypt  password

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
     const { email, password, username } = req.body
     
     try {
+
+       const passwordHash = await bcrypt.hash(password, 10);
+
         const user = await User.create({
             email,
-            password,
+            passwordHash,
             username
         });
 
