136dcc780130d51ef99c85dcc460f096cf92fca0
JoseG1297 <mordo1309@gmail.com>
2025-02-01 20:06:30 -0600

    Relacionar tareas con usuarios durante CRUD

--- backend/src/controllers/tasks.controller.js
+++ backend/src/controllers/tasks.controller.js
@@ -5,7 +5,9 @@ import { httpStatus } from '../libs/staticData.js';
 
 export const getTasks = async (req, res) => {
     try {
-        const tasks = await Task.find();
+        const userId = req.userData.id;
+
+        const tasks = await Task.find({user: userId}).populate('user');
 
         if(!tasks){
             res.status(httpStatus.BAD_REQUEST).json({message: 'Tasks not found'});
@@ -32,7 +34,7 @@ export const getTaskById = async (req, res) => {
         res.status(httpStatus.OK).json(taskFound);
 
     } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error registering user' , error});
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error registering task' , error});
     }
 }
 
@@ -42,9 +44,12 @@ export const createTask = async (req, res) => {
     
     try 
     {
+        const userId = req.userData.id;
+
         const newTask =  new Task({
             tittle,
-            description
+            description,
+            user: userId
         });
 
         const taskCreated = await newTask.save();
@@ -64,17 +69,9 @@ export const updateTask = async (req, res) => {
     try 
     {
         const taskId = req.params.id;
-
-        const taskFound = await User.findById(taskId);
-        
-        if(!taskFound){
-            res.status(httpStatus.NOT_FOUND).json({message: 'Task not found'});
-        }
-
         const taskUpdated = await Task.findByIdAndUpdate(taskId, req.body, {
             new: true
         })
-
         res.status(httpStatus.CREATED).json(taskUpdated);
     } catch (error) {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error update user task' , error: error});
@@ -86,18 +83,9 @@ export const deleteTask = async (req, res) => {
     
     try {
         const taskId = req.params.id;
-
-        const taskFound = await User.findById(taskId);
-        
-        if(!taskFound){
-            res.status(httpStatus.NOT_FOUND).json({message: 'Task not found'});
-        }
-
         const taskDeleted = await Task.findByIdAndDelete(taskId, req.body)
-
-        
         res.status(httpStatus.CREATED).json(taskDeleted);
     } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error registering user' , error});
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error delete task' , error});
     }
 }
--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -12,6 +12,11 @@ const taskSchema = mongoose.Schema({
     date: {
         type: Date,
         default:  Date.now
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 },
 {
