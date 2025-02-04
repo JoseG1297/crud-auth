662ee2a50c296e717923ecc8878c0f6220df5f0b
JoseG1297 <mordo1309@gmail.com>
2025-02-04 16:05:43 -0600

    Primeros paros proteccion de rutas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { RegisterPage } from "./pages/RegisterPage"
 import { LoginPage } from "./pages/LoginPage"
-import { ProfilePage } from "./pages/ProfilePage"
-import { TasksPage } from "./pages/TasksPage"
-import { TaskFormPage } from "./pages/TaskFormPage"
+import { HomePage } from "./pages/HomePage";
+import { ProfilePage } from "./pages/ProfilePage";
+import { TaskFormPage } from "./pages/TaskFormPage";
+import { TasksPage } from "./pages/TasksPage";
+
 
 
 import { AuthProvider } from "./context/AuthContext"
+
+import { ProtectedRoutes } from "./components/ProtectedRoutes"
 
 function App() {
   return (
@@ -18,10 +22,14 @@ function App() {
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/register" element={<RegisterPage/>} />
-          <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/tasks" element={<TasksPage/>} />
-          <Route path="/add-tasks" element={<TaskFormPage/>} />
-          <Route path="/tasks/:id" element={<h1>Task detail page</h1>} />
+
+          <Route element={<ProtectedRoutes/>}>
+            <Route path="/profile" element={<ProfilePage/>} />
+            <Route path="/tasks" element={<TasksPage/>} />
+            <Route path="/task-form" element={<TaskFormPage/>} />
+            <Route path="/" element={<HomePage/>} />
+          </Route>
+
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </BrowserRouter>
