7347b82f53748d441e82ba90592c595ba80f14ae
JoseG1297 <mordo1309@gmail.com>
2025-02-10 14:53:57 -0600

    persisitiendo datos de autenticacion entre rutas

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,9 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authDataReducer from '../slice/authSlice';
 
-import authDataReducer from '../slice/authSlice'
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authDataReducer);
 
 export const store = configureStore({
   reducer: {
-    authData: authDataReducer,
+    authData: persistedAuthReducer,
+    // otros reducers si los tienes
   },
-})
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
